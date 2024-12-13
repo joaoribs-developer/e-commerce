5996436a95bb4f9bc434eccacafbae58e3bc6648
João Vitor <joaoribeiroh1@gmail.com>
2024-12-13 00:22:59 -0300

    finish today

--- aws/scripts/before-install.sh
+++ aws/scripts/before-install.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-set -xe
-
-# Delete the old  directory as needed.
-if [ -d /usr/local/codedeployresources ]; then
-    sudo rm -rf /usr/local/codedeployresources/
-fi
-
-sudo mkdir -vp /usr/local/codedeployresources
+echo ""
