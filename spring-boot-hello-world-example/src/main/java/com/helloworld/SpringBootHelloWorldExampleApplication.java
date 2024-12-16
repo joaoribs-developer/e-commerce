02cc298a1c44a5b6dfdab180d563cee68eede79f
João Vitor <joaoribeiroh1@gmail.com>
2024-12-16 19:19:22 -0300

    a

--- spring-boot-hello-world-example/src/main/java/com/helloworld/SpringBootHelloWorldExampleApplication.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/SpringBootHelloWorldExampleApplication.java
@@ -18,4 +18,5 @@ public class SpringBootHelloWorldExampleApplication  extends SpringBootServletIn
 {
 SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);
 }
+
 }
---
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/repository/DadosDaCompraRepository.java
@@ -0,0 +1,10 @@
+package com.helloworld.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.helloworld.model.DadosDaCompra;
+
+
+@Repository
+public interface DadosDaCompraRepository extends JpaRepository<DadosDaCompra, Long> {
+}
